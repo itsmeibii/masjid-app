60751c2e9432379f90fdeb4c4631a0a8ce4b89e8
itsmeibii <ibi12341@icloud.com>
2024-07-09 00:39:28 -0400

    before work 7/10/24

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { createContext, useState, useEffect, useContext } from 'react';
+import {onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import { auth } from '../backend/firebaseConfig';
+import {doc,getDoc,setDoc} from 'firebase/firestore';
+import bcrypt from 'bcryptjs';
 
 export const AuthContext = createContext();
 export const AuthContextProvider = ({children}) => {
   const [user,setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(undefined);
   const [loading,setLoading] = useState(true);
+  const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  };
     useEffect(() => {
-      //onauthstatechanged
+      const unsub = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setIsAuthenticated(true);
+          setUser(user);
+        } else {
+          setIsAuthenticated(false);
+          setUser(null);
+        }
+        return unsub;
+      });
       
     },[])
     const login = async (email,password) => {
@@ -25,9 +43,21 @@ export const AuthContextProvider = ({children}) => {
         
       }
     }
-    const register = async (email,password, username, profileUrl) => {
+    const register = async (email,password, firstName, lastName ,age,state , city, phoneNumber ) => {
       try {
-        
+        const response = await createUserWithEmailAndPassword(auth, email, password);
+        const user = response.user;
+        const hashedp = await hashPassword(password);
+        await setDoc(doc(db,'users',user.uid), {
+          email: user.email,
+          password: hashedp,
+          "first-name" : firstName,
+          "last-name" : lastName,
+          "phone-number": phoneNumber,
+          age,
+          state,
+          city,
+        }) 
       } catch (error) {
         
       }
